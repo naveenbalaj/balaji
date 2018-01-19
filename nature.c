b365a3342f661f6fd67a082dc46a4cfc6edcdaca
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-01-19 15:52:01 +0530

    Create nature.c

---
+++ nature.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+int main(void)
+{
+	int p,q;
+	int a=0;
+	printf("\n enter the n value");
+	scanf("%d",&p);
+	for(q=0;q<=p;q++)
+	{
+		a=a+p;
+	}
+	printf("\n sum value is %d",a);
+	return 0;
+}
