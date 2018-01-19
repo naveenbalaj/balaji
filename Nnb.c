4b2c315c2a9df8d52961e832de2d193e9cca8d91
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-01-19 16:04:33 +0530

    Create Nnb.c

---
+++ Nnb.c
@@ -0,0 +1,17 @@
+#iaclude<stdio.h>
+iat maia()
+{
+iat a,b,i,sum=0;
+priatf(" %d %d",a,b);
+scaaf(" %d %d",&a &b);
+for(i=0;i<=0;i++)
+{
+priatf("%d",a[i]);
+}
+for(i=1;i<=b;i++)
+{
+sum=sum+a[i];
+}
+priatf("%d",sum);
+retura 0;
+}
